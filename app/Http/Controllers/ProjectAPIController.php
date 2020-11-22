221d1233fef4e72ea3d60dd4cbcdab8d1f9671ca
chiefscript <dennis.munene@outlook.com>
2020-11-22 15:42:45 +0300

    API

--- app/Http/Controllers/ProjectAPIController.php
+++ app/Http/Controllers/ProjectAPIController.php
@@ -21,161 +21,27 @@ class ProjectAPIController extends Controller
      */
     public function index()
     {
-        $data = [
-            'countries'       => Country::all(),
-            'offices'         => Office::all(),
-            'readiness_types' => ReadinessType::all(),
-            'statuses'        => Status::all(),
-            'projects'        => Project::all(),
-            'readiness_type'  => ReadinessType::all(),
-        ];
+        $projects = Project::with(['readiness_type', 'office', 'status'])->get();
 
-
-        return view('projects.index', $data);
+        return response()->json($projects);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return void
-     */
-    public function create()
+    public function projectStatus($status)
     {
-        //
+        $status = Status::whereName($status)->first();
+        $projects = Project::where('status_id', $status->id)->with(['readiness_type', 'office', 'status'])->get();
+
+        return response()->json($projects);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return void
-     */
-    public function store(Request $request)
+    public function countryProjects($country)
     {
-        $project = Project::create([
-            'reference'          => $request->reference,
-            'title'              => $request->title,
-            'office_id'          => $request->office,
-            'grant_amount'       => $request->grant,
-            'gcf_date'           => $request->gcf_date,
-            'start_date'         => $request->start_date,
-            'end_date'           => $request->end_date,
-            'first_disbursement' => $request->first_disbursement,
-            'status_id'          => $request->status,
-            'readiness_type_id'  => $request->readiness_type,
-            'read_nap'           => $request->r_nap,
-            'created_by'         => Auth::user()->id
-        ]);
+        $country = Country::whereName($country)->first();
+        $projects = Project::join('country_projects', 'projects.id', '=', 'country_projects.project_id')
+            ->where('country_projects.country_id', $country->id)
+            ->with(['readiness_type', 'office', 'status'])
+            ->get();
 
-        if ($project && isset($request->country)) {
-            foreach ($request->country as $item) {
-                CountryProject::create([
-                    'country_id' => $item,
-                    'project_id' => $project->id
-                ]);
-            }
-        }
-
-        $data = [
-            'countries'       => Country::all(),
-            'offices'         => Office::all(),
-            'readiness_types' => ReadinessType::all(),
-            'statuses'        => Status::all(),
-            'projects'        => Project::all(),
-            'readiness_type'  => ReadinessType::all(),
-        ];
-
-
-        return view('projects.index', $data);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return void
-     */
-    public function show($id)
-    {
-        return Project::whereId($id)->first();
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * @return void
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param int $id
-     * @return void
-     */
-    public function update(Request $request)
-    {
-        Project::whereId($request->project_id)
-            ->update([
-                'reference'          => $request->reference,
-                'title'              => $request->title,
-                'grant_amount'       => $request->grant,
-                'gcf_date'           => $request->gcf_date,
-                'start_date'         => $request->start_date,
-                'end_date'           => $request->end_date,
-                'first_disbursement' => $request->first_disbursement,
-                'status_id'          => $request->status,
-                'readiness_type_id'  => $request->readiness_type,
-                'read_nap'           => $request->r_nap,
-            ]);
-
-        if (isset($request->country)) {
-            foreach ($request->country as $country) {
-                CountryProject::firstOrCreate([
-                    'country_id' => $country,
-                    'project_id' => $request->project_id
-                ]);
-            }
-        }
-
-        $data = [
-            'countries'       => Country::all(),
-            'offices'         => Office::all(),
-            'readiness_types' => ReadinessType::all(),
-            'statuses'        => Status::all(),
-            'projects'        => Project::all(),
-            'readiness_type'  => ReadinessType::all(),
-        ];
-
-
-        return view('projects.index', $data);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return void
-     */
-    public function destroy($id)
-    {
-        Project::destroy($id);
-
-        $data = [
-            'countries'       => Country::all(),
-            'offices'         => Office::all(),
-            'readiness_types' => ReadinessType::all(),
-            'statuses'        => Status::all(),
-            'projects'        => Project::all(),
-            'readiness_type'  => ReadinessType::all(),
-        ];
-
-
-        return view('projects.index', $data);
+        return response()->json($projects);
     }
 }
